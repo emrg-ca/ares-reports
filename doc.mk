26a13c2ca0c1949d8a193f3728f8229df1e13666
Tyler Tidman <tyler.tidman@draak.ca>
2014-05-12 18:40:09 -0400

    Correct the logic for the doc makefile helpers to make it prefer pure text

--- doc.mk
+++ doc.mk
@@ -3,38 +3,17 @@ SHELL := /bin/bash
 .PHONY : all
 all :
 
-%.pdf : %.odt
-	@libreoffice --headless --convert-to pdf $^ \
-  "-env:UserInstallation=file:///tmp/libreofficebug"
+%.pdf : %.txt
+	@pdftotext $^
 
-%.pdf : %.odp
-	@libreoffice --headless --convert-to pdf $^ \
-  "-env:UserInstallation=file:///tmp/libreofficebug"
-
-.PRECIOUS : %.odt
 %.odt : %.doc
-	@libreoffice --headless --convert-to odt $^ \
+	@libreoffice --headless --convert-to txt:Text $^ \
   "-env:UserInstallation=file:///tmp/libreofficebug"
 
-%.odt : %.docx
-	@libreoffice --headless --convert-to odt $^ \
+%.doc : %.txt
+	@libreoffice --headless --convert-to txt:Text $^ \
   "-env:UserInstallation=file:///tmp/libreofficebug"
 
-.PRECIOUS : %.odp
-%.odp : %.ppt
-	@libreoffice --headless --convert-to odp $^ \
+%.docx : %.ppt
+	@libreoffice --headless --convert-to txt:Text $^ \
   "-env:UserInstallation=file:///tmp/libreofficebug"
-
-%.odp : %.pptx
-	@libreoffice --headless --convert-to odp $^ \
-  "-env:UserInstallation=file:///tmp/libreofficebug"
-
-# Word Template from OpenDocument Text Template
-#%.dot : %.otd
-#	@libreoffice --headless --convert-to dot $^ \
-# "-env:UserInstallation=file:///tmp/libreofficebug"
-
-# Powerpoint Template from OpenDocument Presentation Template
-#%.pot : %.otp
-#	@libreoffice --headless --convert-to pot $^ \
-# "-env:UserInstallation=file:///tmp/libreofficebug"
